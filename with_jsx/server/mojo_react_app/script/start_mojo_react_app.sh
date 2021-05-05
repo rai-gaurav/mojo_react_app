678520fa10ada24aa559fcf754462a7f3e3920fa
rai-gaurav <gauravrai7860@gmail.com>
2021-05-05 14:58:04 +0530

    Added deployment receipe

---
+++ with_jsx/server/mojo_react_app/script/start_mojo_react_app.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# This script can be runned by docker or directly by provide the parameter
+# ./start_myapp.sh -m "development" or
+# ./start_myapp.sh -m "production"
+
+# Restart Web server (here Apache)
+service apache2 restart
+
+# start Mojo React App
+while getopts "m:" opt; do
+    case ${opt} in
+        m)
+            mode="${OPTARG}"
+            echo "Running in '$mode' mode"
+            if [ "$mode" == "development" ]; then
+                # Since we are using uwsgi, we have commented the morbo
+                # exec morbo -l "https://*:7070" script/myapp
+                exec uwsgi --ini etc/uwsgi.conf:$mode
+            elif [[ "$mode" = "production" || "$mode" = "staging" ]]; then
+                # exec hypnotoad -f script/myapp
+                exec uwsgi --ini etc/uwsgi.conf:$mode
+            else 
+                echo "Wrong mode provided. Accepted value - development, staging or production" 1>&2
+            fi
+            ;;
+        : )
+            echo "Invalid option: $OPTARG requires an argument" 1>&2
+    esac
+done
