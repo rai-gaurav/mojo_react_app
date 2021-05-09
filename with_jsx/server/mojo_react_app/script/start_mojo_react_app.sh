b4cc3e9f4155d615b8aef624749a9d43df2ccf3e
rai-gaurav <gauravrai7860@gmail.com>
2021-05-09 15:14:41 +0530

    Updated dockerfile and Makefile

--- with_jsx/server/mojo_react_app/script/start_mojo_react_app.sh
+++ with_jsx/server/mojo_react_app/script/start_mojo_react_app.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# This script can be runned by docker or directly by provide the parameter
-# ./start_myapp.sh -m "development" or
-# ./start_myapp.sh -m "production"
+# This script can be runned by docker or directly by provide the parameter.
+# Move it outside and run -
+# ./start_mojo_react_app.sh -m "development" or
+# ./start_mojo_react_app.sh -m "production"
 
 # Restart Web server (here Apache)
 service apache2 restart
@@ -14,10 +15,10 @@ while getopts "m:" opt; do
             echo "Running in '$mode' mode"
             if [ "$mode" == "development" ]; then
                 # Since we are using uwsgi, we have commented the morbo
-                # exec morbo -l "https://*:7070" script/myapp
+                # exec morbo -l "https://*:6363" script/mojo_react_app
                 exec uwsgi --ini etc/uwsgi.conf:$mode
             elif [[ "$mode" = "production" || "$mode" = "staging" ]]; then
-                # exec hypnotoad -f script/myapp
+                # exec hypnotoad -f script/mojo_react_app
                 exec uwsgi --ini etc/uwsgi.conf:$mode
             else 
                 echo "Wrong mode provided. Accepted value - development, staging or production" 1>&2
